0d3c540cd74cf295f9e89830367c351fae5280c7
Soo Fatt <soofatt93@gmail.com>
2014-02-22 04:34:26 +0800

    Add test for list_removeLast for 3 elements

--- test/test_LinkedList.c
+++ test/test_LinkedList.c
@@ -60,12 +60,34 @@ void test_list_removeLast_should_remove_last_element_testing_with_2_elements(){
 	list->head = &arrayElem[0];
 	list->tail = &arrayElem[(list->length) - 1];
 	
-	printf("Addr of arrayElem[0]: %p\n", &arrayElem[0]);
-	printf("Addr of arrayElem[1]: %p\n", &arrayElem[1]); //Address of returned element
+	//printf("Addr of arrayElem[0]: %p\n", &arrayElem[0]);
+	//printf("Addr of arrayElem[1]: %p\n", &arrayElem[1]); //Address of returned element
 	
 	TEST_ASSERT_EQUAL_PTR(&arrayElem[1], list_removeLast(list));
 	TEST_ASSERT_EQUAL_PTR(&arrayElem[0], list->head);
 	TEST_ASSERT_EQUAL_PTR(&arrayElem[(list->length) - 1], list->tail);
 	TEST_ASSERT_EQUAL(1, list->length);
+	
+}
+
+void test_list_removeLast_should_remove_last_element_testing_with_3_elements(){
+	LinkedList *list;
+	list = createLinkedList();
+	
+	Element arrayElem[] = {{.next = &arrayElem[1], .data = 1}, 
+							{.next = &arrayElem[2], .data = 2}, 
+							{.next = NULL, .data = 3}};
+	list->length = 3;
+	list->head = &arrayElem[0];
+	list->tail = &arrayElem[(list->length) - 1];
+	
+	printf("Addr of arrayElem[0]: %p\n", &arrayElem[0]);
+	printf("Addr of arrayElem[1]: %p\n", &arrayElem[1]);
+	printf("Addr of arrayElem[2]: %p\n", &arrayElem[2]); //Address of returned element
+	
+	TEST_ASSERT_EQUAL_PTR(&arrayElem[2], list_removeLast(list));
+	TEST_ASSERT_EQUAL_PTR(&arrayElem[0], list->head);
+	TEST_ASSERT_EQUAL_PTR(&arrayElem[(list->length) - 1], list->tail);
+	TEST_ASSERT_EQUAL(2, list->length);
 	
 }
